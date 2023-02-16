748ebd03123056512528444363d4ba8594f72d48
bczhc <bczhc0@126.com>
2023-02-16 16:12:29 +0800

    add `rime_struct` macro
    like the original `RIME_STRUCT` macro in
    `rime_api.h`

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,28 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[macro_export]
+macro_rules! rime_struct {
+    ($var:ident : $t:ty) => {
+        let mut $var = std::mem::MaybeUninit::<$t>::zeroed();
+        let mut $var = unsafe { $var.assume_init_mut() };
+        $var.data_size =
+            (std::mem::size_of::<$t>() - std::mem::size_of_val(&$var.data_size)) as std::ffi::c_int;
+    };
+}
+
+#[cfg(test)]
+mod test {
+    use crate::RimeTraits;
+    use std::mem;
+
+    #[test]
+    fn test() {
+        rime_struct!(traits: RimeTraits);
+        assert_eq!(
+            traits.data_size as usize,
+            mem::size_of::<RimeTraits>() - mem::size_of_val(&traits.data_size)
+        );
+    }
+}
